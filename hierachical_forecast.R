5a7c7b16b7d0a96c8f8f3f844ae6aef9a9074830
clfo2014@gmail.com <clfo2014@gmail.com>
2022-11-11 15:17:58 -0500

    #02 modeling section; baseline complete

--- hierachical_forecast.R
+++ hierachical_forecast.R
@@ -127,6 +127,172 @@ train_raw_tbl %>%
 
 
 
+# ******************************************************************************
+# DATA PREP FOR MODELING ----
+# ******************************************************************************
+
+# Summarize by Time (All Hierarchy) ----
+daily_hierachical_tbl <- train_raw_tbl %>% 
+    group_by(country, store, product) %>% 
+    summarize_by_time(date, "day", num_sold = sum(num_sold)) %>% 
+    ungroup() %>% 
+    select(date, everything(.))
+
+# Params ----
+FORECAST_TIMEFRAME <- "day"
+
+FORECAST_HORIZON  <- 90
+
+# Full Data ----
+full_data_tbl <- train_raw_tbl %>% 
+    
+    # fix data types
+    mutate_if(is.character, as.factor) %>% 
+    
+    # add hierarchical aggregations
+    select(-row_id) %>% 
+    add_column(all_countries_id = "all_countries", .before = 1) %>% 
+    pivot_longer(
+        cols      = c(all_countries_id, country, store, product),
+        names_to  = "hierachy",
+        values_to = "identifier"
+    ) %>% 
+    group_by(hierachy, identifier, date) %>% 
+    summarise(num_sold = sum(num_sold, na.rm = TRUE)) %>% 
+    ungroup() %>% 
+    
+    # transformations
+    mutate(num_sold = log(num_sold)) %>% 
+    
+    # Apply Time Series Feature Engineering
+    group_by(hierachy, identifier) %>% 
+    pad_by_time(date, .by = FORECAST_TIMEFRAME, .pad_value = 0) %>% 
+    
+    # Extend Into Future
+    future_frame(date, .length_out = FORECAST_HORIZON, .bind_data = TRUE) %>% 
+    
+    # Add Time Series Features
+    tk_augment_fourier(date, .periods = c(7, 14, 21, 28)) %>% 
+    tk_augment_lags(num_sold, .lags = FORECAST_HORIZON) %>% 
+    tk_augment_slidify(
+        .value   = num_sold_lag90,
+        .f       = ~ mean(.x, na.rm = TRUE),
+        .period  = c(7, 14, 21, 28),
+        .partial = TRUE,
+        .align   = "center"
+    ) %>% 
+    ungroup() %>% 
+    rowid_to_column(var = "row_id")
+
+full_data_tbl %>% glimpse()
+
+skimr::skim(full_data_tbl)
+
+# * Data Prepared ----
+data_prepared_tbl <- full_data_tbl %>% 
+    filter(! is.na(num_sold)) %>% 
+    drop_na()
+
+# * Future Data ----
+future_data_tbl <- full_data_tbl %>% 
+    filter(is.na(num_sold))
+
+
+# ******************************************************************************
+# TIME SPLIT ----
+# ******************************************************************************
+splits <- data_prepared_tbl %>% 
+    time_series_split(date, assess = FORECAST_HORIZON, cumulative = TRUE)
+
+train_tbl <- training(splits)
+test_tbl  <- testing(splits)
+
+splits %>% 
+    tk_time_series_cv_plan() %>% 
+    plot_time_series_cv_plan(date, num_sold)
+
+
+# ******************************************************************************
+# CLEAN DATA (REMOVE OUTLIERS) ----
+# ******************************************************************************
+
+train_cleaned_tbl <- train_tbl %>% 
+    group_by(hierachy, identifier) %>% 
+    mutate(num_sold = ts_clean_vec(num_sold, period = 3)) %>% 
+    ungroup() 
+
+train_cleaned_tbl %>% 
+    filter(hierachy == "country" & identifier == "France") %>% 
+    plot_anomaly_diagnostics(date, num_sold)
+
+
+
+# ******************************************************************************
+# RECIPE ----
+# ******************************************************************************
+
+recipe_spec <- recipe(num_sold ~., data = train_cleaned_tbl) %>% 
+    step_rm(row_id) %>% 
+    step_timeseries_signature(date) %>% 
+    step_rm(matches("(.iso)|(.xts)|(hour)|(minute)(second)(am.pm)")) %>% 
+    step_dummy(all_nominal(), one_hot = TRUE) %>% 
+    step_normalize(date_index.num, date_year)
+
+recipe_spec %>% prep() %>% juice() %>% glimpse()
+
+# ******************************************************************************
+# MODELING ----
+# ******************************************************************************
+
+# * Xgboost ----
+wflw_fit_xgboost <- workflow() %>% 
+    add_model(spec = boost_tree() %>% set_mode("regression") %>% set_engine("xgboost")) %>% 
+    add_recipe(recipe_spec %>% step_rm(date)) %>% 
+    fit(train_cleaned_tbl)
+
+# * Random Forest ----
+wflw_fit_ranger <- workflow() %>% 
+    add_model(rand_forest() %>% set_mode("regression") %>% set_engine("ranger")) %>% 
+    add_recipe(recipe_spec %>% step_rm(date)) %>% 
+    fit(train_cleaned_tbl)
+
+# * MARS ----
+wflw_fit_mars <- workflow() %>% 
+    add_model(mars() %>% set_mode("regression") %>% set_engine("earth")) %>% 
+    add_recipe(recipe_spec %>% step_rm(date)) %>% 
+    fit(train_cleaned_tbl)
+
+# * Cubist Model ----
+wflw_fit_cubsist <- workflow() %>% 
+    add_model(cubist_rules("regression") %>% set_engine("Cubist")) %>% 
+    add_recipe(recipe_spec %>% step_rm(date)) %>% 
+    fit(train_cleaned_tbl)
+
+
+# ******************************************************************************
+# MODELTIME TABLE ----
+# ******************************************************************************
+modeltime_table_fit <- modeltime_table(
+    wflw_fit_xgboost,
+    wflw_fit_ranger,
+    wflw_fit_mars,
+    wflw_fit_cubsist
+)
+
+calibration_fit_tbl <- modeltime_table_fit %>% 
+    modeltime_calibrate(test_tbl) %>% 
+    modeltime_accuracy() %>% 
+    arrange((rmse))
+
+test_tbl %>% 
+    summarise(
+        mean   = mean(num_sold),
+        median = median(num_sold),
+        sd     = sd(num_sold)
+    )
+
+
+
 
 
 
